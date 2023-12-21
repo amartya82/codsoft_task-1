c798a30d55a2b608786a6a4ca5dc0f79c35ed176
amartya82 <101491357+amartya82@users.noreply.github.com>
2023-12-21 09:46:05 +0530

    Update task 1.cpp

--- task 1.cpp
+++ task 1.cpp
@@ -3,21 +3,18 @@
 #include <ctime>
 
 int main() {
-    // Seed the random number generator
-    srand(static_cast<unsigned int>(time(0)));
     
-    // Generate a random number between 1 and 100
+    srand(static_cast<unsigned int>(time(0)));
+
     int secretNumber = rand() % 100 + 1;
     int guess, attempts = 0;
     
     std::cout << "Welcome to the Number Guessing Game! \n" << std::endl;
-    
-    // Keep asking the user to guess until they get the correct number
+
     while (true) {
         std::cout << "Guess the number (between 1 and 100): ";
         std::cin >> guess;
-        
-        // Check if the guess is correct
+    
         if (guess == secretNumber) {
             std::cout << "Congratulations! You guessed the correct number." << std::endl;
             break;
